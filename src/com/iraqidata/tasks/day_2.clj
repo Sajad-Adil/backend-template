6bc78ad54117ecff4dd7543b2807036a8fe4689b
Sajad_Adil <kinnkako72@gmail.com>
2024-08-21 13:02:34 +0300

    solutions for day 2 task

--- src/com/iraqidata/tasks/day_2.clj
+++ src/com/iraqidata/tasks/day_2.clj
@@ -1,7 +1,7 @@
 (ns com.iraqidata.tasks.day-2
   (:require
    [clojure.string :as str]
-   scicloj.clay.v2.api
+   '[scicloj.clay.v2.api :as clay]
    [tablecloth.api :as tc]
    tech.v3.datatype.casting))
 
@@ -9,15 +9,34 @@
                     {:key-fn #(keyword (str/replace (name %) "_" "-"))}))
 
 ;; 1. How many flights were there in total?
-
+(def flights-total
+  (tc/row-count ds))
+flights-total
 
 ;; 2. How many unique carriers were there in total?
-
+(def total-unique-carriers
+  (tc/row-count (tc/unique-by ds :carrier))
+  )
+total-unique-carriers
 
 ;; 3. How many unique airports were there in total?
-
+(def total-unique-airports
+  (-> ds
+      (tc/select-columns [:origin :dest])
+      (tc/unique-by)
+      (tc/row-count)))
+total-unique-airports
+;; without thread
+;; (def total-unique-airports
+;; (tc/row-count
+;;  (tc/unique-by ds [:origin :dest])))
+;; total-unique-airports
 
 ;; 4. What is the average arrival delay for each month?
+(def avg-arrival-delay-by-month
+ ( -> ds
+  (tc/group-by [:month])
+  (tc/mean :arr-delay))
 
 
 ;; Optional: Use the `airlines` dataset to get the name of the carrier with the
@@ -26,3 +45,13 @@
 (def airlines
   (tc/dataset "./resources/data/airlines.csv"
               {:key-fn keyword}))
+  
+(def carrier-highest-avg-distance
+  (-> ds
+      (tc/group-by [:carrier])
+      (tc/mean :distance)
+      (tc/inner-join airlines {:on [:carrier :carrier]})
+      (tc/order-by [:avg-distance] :desc)
+      (tc/select-columns [:name :avg-distance])
+      (tc/first)))
+ carrier-highest-avg-distance
